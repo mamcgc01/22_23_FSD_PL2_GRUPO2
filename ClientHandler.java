29ff6a6573e62bbdf7284804fbcdf6b2adb3ad44
mamcgc01 <miguel.caldas99@gmail.com>
2022-10-13 12:00:41 +0100

    Update

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,8 +1,17 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+import java.nio.Buffer;
 import java.util.*;
 
 public class ClientHandler implements Runnable {
 
     public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bf;
+    private BufferedWriter bw;
+    private String clientUsername;
 
     @Override
     public void run() {
